77f1f550106d612617a3a1dbc288580cc301c2f4
Valerie Robateau <valerie.robateau@ed-fi.org>
2021-08-10 13:27:43 -0500

    Devop - 60 update

--- .teamcity/_self/buildFeatures/PreRelease.kt
+++ .teamcity/_self/buildFeatures/PreRelease.kt
@@ -6,6 +6,9 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.swabra
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetBuild
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetPublish
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetTest
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 object PreRelease : Project({
     name = "Pre-Release"
