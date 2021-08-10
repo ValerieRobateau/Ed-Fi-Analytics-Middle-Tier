10d7eded6ebcf8931f20b1bde632428b7ee49efa
Valerie Robateau <valerie.robateau@ed-fi.org>
2021-08-10 14:57:10 -0500

    update to template

--- .teamcity/_self/buildFeatures/PreRelease.kt
+++ .teamcity/_self/buildFeatures/PreRelease.kt
@@ -1,14 +1,13 @@
-package _self.Project
+package _self.projects
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.swabra
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetBuild
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetPublish
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetTest
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 object PreRelease : Project({
     name = "Pre-Release"
